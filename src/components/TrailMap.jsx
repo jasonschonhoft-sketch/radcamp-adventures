19e7a9b7c408405e1df47e8fdf33ddcd6394b587
jasonschonhoft-1906 <jason.schonhoft@gmail.com>
2026-05-19 11:41:23 -0600

    fix: resolve all map UI overlaps

--- src/components/TrailMap.jsx
+++ src/components/TrailMap.jsx
@@ -264,7 +264,7 @@ export default function TrailMap({ activeFilters, singletrackOnly, onMapReady })
         gestureHandling="greedy"
         mapTypeId="hybrid"
         mapTypeControl={true}
-        mapTypeControlOptions={{ position: 3 }}
+        mapTypeControlOptions={{ position: 9 }}
         fullscreenControl={false}
         streetViewControl={false}
         zoomControl={true}
