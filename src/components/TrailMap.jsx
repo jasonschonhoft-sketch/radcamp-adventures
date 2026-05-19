1e7735ac0ed0a22a05bdfea9370e128f13291304
jasonschonhoft-1906 <jason.schonhoft@gmail.com>
2026-05-19 13:21:29 -0600

    fix: map type control bottom left

--- src/components/TrailMap.jsx
+++ src/components/TrailMap.jsx
@@ -192,6 +192,7 @@ export default function TrailMap({ activeFilters, singletrackOnly, onMapReady })
         gestureHandling="greedy"
         mapTypeId="hybrid"
         mapTypeControl={true}
+        mapTypeControlOptions={{ position: window.google?.maps?.ControlPosition?.BOTTOM_LEFT ?? 6 }}
         fullscreenControl={false}
         streetViewControl={false}
         zoomControl={true}
